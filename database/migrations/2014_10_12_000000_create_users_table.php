079c9dd3ce45293d6e3ae258f8e5ef54eec86d38
TranDangSy <dangsy1998@gmail.com>
2019-12-17 10:50:10 +0700

    Init project

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('gender');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('phone');
+            $table->string('addresss');
+            $table->integer('status');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
