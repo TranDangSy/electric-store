c7c6b02ceecaeefe8a3718b5a98e994511261c2c
TranDangSy <dangsy1998@gmail.com>
2019-12-23 00:56:33 +0700

    Add layout

---
+++ resources/views/admin/widget/footer.blade.php
@@ -0,0 +1,9 @@
+<script src="../admin_asset/js/jquery.min.js"></script>
+<script src="../admin_asset/js/bootstrap.bundle.min.js"></script>
+<script src="../admin_asset/js/jquery.easing.min.js"></script>
+<script src="../admin_asset/js/sb-admin-2.min.js"></script>
+<script src="../admin_asset/js/Chart.min.js"></script>
+<script src="../admin_asset/js/demo/chart-area-demo.js"></script>
+<script src="../admin_asset/js/demo/chart-pie-demo.js"></script>
+</body>
+</html>
